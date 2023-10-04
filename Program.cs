fc1945d9ed933ac2b95df1ab7b1f6b2263204477
Patrick5432 <112853910+Patrick5432@users.noreply.github.com>
2023-10-04 23:29:07 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+foreach(Process process in Process.GetProcesses())
+{
+    Console.WriteLine($"ID: {process.Id}  Name: {process.ProcessName}");
+}
